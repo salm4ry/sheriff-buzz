3c2003bf07495bb6ea1b3c70d9c1bca578895cbd
Salma Berriche <salma.berriche@gmail.com>
2025-01-12 14:43:32 +0000

    Update alert database logging commands
    reflect changes in packet_data.h so reference is up to date

--- database/log_cmds.sql
+++ database/log_cmds.sql
@@ -1,13 +1,14 @@
--- flag-based alerts: Xmas, FIN, NULL, SYN (TODO)
+-- flag-based alerts: Xmas, FIN, NULL (packet threshold-based)
 INSERT INTO log (fingerprint, dst_port, alert_type, src_ip, packet_count, first, latest)
 	VALUES ('%s', '%d', %d, '%s', %d, to_timestamp(%ld), to_timestamp(%ld))
-	ON CONFLICT (fingerprint, alert_type) DO UPDATE
-		SET packet_count=%d, latest=to_timestamp(%ld)
-		WHERE %d >= log.packet_count AND to_timestamp(%ld) >= log.latest;
+	ON CONFLICT (src_ip, fingerprint, alert_type) WHERE fingerprint IS NOT NULL
+		DO UPDATE SET packet_count=%d, latest=to_timestamp(%ld)
+		WHERE %d > log.packet_count AND to_timestamp(%ld) > log.latest;
 
--- port-based alerts
+
+-- port-based alerts (port threshold-based)
 INSERT INTO log (dst_port, alert_type, src_ip, port_count, first, latest)
 	VALUES ('%s', %d, '%s', %d, to_timestamp(%ld), to_timestamp(%ld))
-	ON CONFLICT (src_ip, alert_type) DO UPDATE
-		SET port_count=%d, dst_port='%s' latest=to_timestamp(%ld)
-		WHERE to_timestamp(%ld) >= log.latest;
+	ON CONFLICT (src_ip, alert_type) WHERE fingerprint IS NULL
+		DO UPDATE SET port_count=%d, dst_port='%s', latest=to_timestamp(%ld)
+		WHERE %d > log.port_count AND to_timestamp(%ld) > log.latest

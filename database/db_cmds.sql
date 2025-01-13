a03bfbbc0eba5291e91013c2688b4a25b1e47a63
Salma Berriche <salma.berriche@gmail.com>
2025-01-13 12:30:07 +0000

    Add alert count SQL query
    - log_cmds.sql -> db_cmds.sql (this file is only for reference anyway)
    - look up source IP in database and return number of alerts

--- database/db_cmds.sql
+++ database/db_cmds.sql
@@ -1,4 +1,4 @@
--- flag-based alerts: Xmas, FIN, NULL (packet threshold-based)
+-- log flag-based alerts: Xmas, FIN, NULL (packet threshold-based)
 INSERT INTO log (fingerprint, dst_port, alert_type, src_ip, packet_count, first, latest)
 	VALUES ('%s', '%d', %d, '%s', %d, to_timestamp(%ld), to_timestamp(%ld))
 	ON CONFLICT (src_ip, fingerprint, alert_type) WHERE fingerprint IS NOT NULL
@@ -6,9 +6,12 @@ INSERT INTO log (fingerprint, dst_port, alert_type, src_ip, packet_count, first,
 		WHERE %d > log.packet_count AND to_timestamp(%ld) > log.latest;
 
 
--- port-based alerts (port threshold-based)
+-- log port-based alerts (port threshold-based)
 INSERT INTO log (dst_port, alert_type, src_ip, port_count, first, latest)
 	VALUES ('%s', %d, '%s', %d, to_timestamp(%ld), to_timestamp(%ld))
 	ON CONFLICT (src_ip, alert_type) WHERE fingerprint IS NULL
 		DO UPDATE SET port_count=%d, dst_port='%s', latest=to_timestamp(%ld)
 		WHERE %d > log.port_count AND to_timestamp(%ld) > log.latest
+
+-- look up number of alerts for a given source IP
+SELECT count(id) FROM log WHERE src_ip = '%s';

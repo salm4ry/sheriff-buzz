e5ef0135ee58624ca6847841e415908427f532eb
Salma Berriche <salma.berriche@gmail.com>
2025-01-16 19:04:21 +0000

    Add bpf_csum_diff() explanation
    based on official docs:
    https://docs.ebpf.io/linux/helper-function/bpf_csum_diff/

--- src/include/patch_header.h
+++ src/include/patch_header.h
@@ -6,44 +6,6 @@
 
 #include <unistd.h>
 
-/**
- * Calculate IP header checksum for count bytes beginning at location addr
- *
- * addr = 16-bit (unsigned short) starting location
- * count = 32-bit (unsigned int) accumulator
- */
-/*
-__sum16 calc_checksum(__u16 *addr, __u16 count)
-{
-	register __u32 sum = 0;
-
-	// sum all 16-bit (unsigned short) words
-	while (count > 1) {
-		sum += *addr++;
-
-		if (count >= 2)
-			count -= 2;
-		else
-			break;
-	}
-
-	// TODO overflow check (unsigned int)
-
-	// pad and add left-over bytes if any
-	if (count != 0) {
-		sum += * (__u8 *) addr;
-	}
-
-	// fold 32-bit sum to 16 bits
-	while (sum>>16) {
-		sum = (sum & 0xffff) + (sum >> 16);
-	}
-
-	// ~ = bitwise NOT
-	return ~sum;
-}
-*/
-
 static __always_inline __u16 fold(__u64 sum)
 {
 	for (int i = 0; i < 4; i++) {
@@ -56,8 +18,6 @@ static __always_inline __u16 fold(__u64 sum)
 
 /**
  * Set checksum of IP header
- *
- * original and calculated checksums should be equal!
  */
 void ip_checksum(struct iphdr *iph)
 {
@@ -65,7 +25,12 @@ void ip_checksum(struct iphdr *iph)
 	iph->check = 0;
 
 	/*
-	 * compute a checksum different from 
+	 * compute a checksum difference from raw buffer pointed to by from (size
+	 * from_size) towards raw buffer pointed to by to (size to_size) + seed
+	 * (optional)
+	 *
+	 * bpf_csum_diff(__be32 *from, __u32 from_size,
+	 * 				 __be32 *to, __u32 to_size, __wsum seed)
 	 */
 	__u64 sum = bpf_csum_diff(0, 0, (unsigned int *) iph, sizeof(struct iphdr), 0);
 	__u16 csum = fold(sum);
@@ -80,12 +45,13 @@ void ip_checksum(struct iphdr *iph)
 /**
  * Patch IP header destination IP address and recompute header checksum
  *
- * iph: IP headers to patch
+ * iph: IP header to patch
  * dst_ip: destination IP to use
  */
 static inline void change_dst_addr(struct iphdr *iph, __be32 dst_ip)
 {
 	iph->daddr = dst_ip;
+
 	/* set checksum to 0 before calculation */
 	iph->check = 0;
 	ip_checksum(iph);

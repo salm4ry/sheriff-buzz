d56c6da051d18a864bc764520f21ac72416fdf38
Salma Berriche <salma.berriche@gmail.com>
2025-02-27 01:57:50 +0000

    Refactor port-based test script
    - check for config file changes before continuing
    - remove database and log file checks (to be replaced with separate
      script)

--- testing/port-based-test.sh
+++ testing/port-based-test.sh
@@ -5,14 +5,6 @@ LS=/usr/bin/ls
 HEAD=/usr/bin/head
 NMAP=/usr/bin/nmap
 
-# database query: inner join with alert type to get human-readable names
-CHECK_QUERY="SELECT COUNT(*) FROM scan_alerts WHERE alert_type = $PORT_SCAN_ALERT;"
-LOG_QUERY="SELECT scan_alerts.dst_port, scan_alerts.src_ip,
-	scan_alerts.packet_count, scan_alerts.latest,
-	alert_type.description AS alert_type FROM scan_alerts
-	INNER JOIN alert_type ON scan_alerts.alert_type = alert_type.id
-	WHERE alert_type = ${PORT_SCAN_ALERT};"
-
 DB_NAME="sheriff_logbook"
 PORT_SCAN_ALERT=4
 # NOTE: have to run sheriff-buzz with -c config.json
@@ -50,7 +42,7 @@ do
 	esac
 done
 
-root_dir="/home/$USER/sheriff-buzz"
+root_dir="/home/$user/sheriff-buzz"
 log_dir="$root_dir"/log
 config_path="$root_dir"/config
 
@@ -111,58 +103,12 @@ run_on_host "echo \
 	\"alert_threshold\": 10
 }' > ${config_path}/${CONFIG_FILE}"
 
-# initial alert count
-initial_count=$(count_alerts)
-printf "initial alert count: %d\n" "$initial_count"
+while ! run_on_host "grep -q ${num_ports} ${config_path}/${CONFIG_FILE}"
+do
+	sleep 1
+done
+
+echo "config updated on disk"
 
 printf "nmap scan on %s\n" "${host}"
 nmap_scan "${host}"
-# TODO timing measurement for how long it took the alert to make it to the log
-# file + database
-
-# get log filename
-log=$(get_log_file)
-printf "waiting for logged alert in %s...\n" "$log"
-
-# wait until alert has made it into the log file before continuing
-# TODO set timeout
-while true
-do
-	if ! check_log
-	then
-		run_on_host "tail -1 $log_dir/$log"
-		sleep 1;
-	else
-		# NOTE log file timing measurement ends here
-		break
-	fi
-done
-
-
-# save log to temporary file
-tmp_log=$(mktemp -p .)
-run_on_host "cat ${log_dir}/$log" > "${tmp_log}"
-# only output alert-related lines
-grep -m1 'alert: ' "${tmp_log}"
-# remove temporary file after use
-rm "${tmp_log}"
-
-printf "\nconnecting to %s alert database\n" "${host}"
-
-# wait until alert is in database before printing contents
-# TODO set timeout
-while true
-do
-	count=$(count_alerts)
-
-	if [[ "$count" > "$initial_count" ]]
-	then
-		# NOTE database timing measurement ends here
-		break
-	else
-		printf "number of alerts: %d\n" "$count"
-		sleep 1
-	fi
-done
-
-run_on_host "psql ${DB_NAME} -c '${LOG_QUERY}'"

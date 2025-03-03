d37784734a7e47a80f7a73543bf0ab5b2e7031ed
Salma Berriche <salma.berriche@gmail.com>
2025-03-03 18:05:29 +0000

    Finish interface snippet
    get interface name from address

--- snippets/interfaces.c
+++ snippets/interfaces.c
@@ -1,32 +1,57 @@
 #include <stdio.h>
-#include <errno.h>
-#include <ifaddrs.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <errno.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ifaddrs.h>
 
 int main(int argc, char *argv[])
 {
-    struct ifaddrs *current_address, *addresses;
-    if (getifaddrs(&addresses) == -1) {
+    struct ifaddrs *current_interface, *interfaces;
+	char iface_addr[NI_MAXHOST], target_addr[NI_MAXHOST];
+	sa_family_t family;
+	int res;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <address>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+    if (getifaddrs(&interfaces) == -1) {
         perror("getifaddrs");
         exit(errno);
     }
 
+	strncpy(target_addr, argv[1], sizeof(target_addr));
 
-    /* list network interfaces
-     * adapted from: https://www.cyberithub.com/list-network-interfaces
-     */
-    current_address = addresses;
-    while (current_address) {
-        sa_family_t family = current_address->ifa_addr->sa_family;
+    current_interface = interfaces;
+    while (current_interface) {
+        family = current_interface->ifa_addr->sa_family;
+
         if (family == AF_INET) {
-            printf("interface: %s\n", current_address->ifa_name);
+			res = getnameinfo(current_interface->ifa_addr, sizeof(struct sockaddr_in),
+					iface_addr, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+			if (res != 0) {
+				fprintf(stderr, "getnameinfo: %s\n", gai_strerror(res));
+				exit(res);
+			}
+
+			if (strncmp(target_addr, iface_addr, NI_MAXHOST) == 0) {
+				printf("interface %s has address %s\n",
+						current_interface->ifa_name, iface_addr);
+				freeifaddrs(interfaces);
+				exit(EXIT_SUCCESS);
+			}
         }
 
-        current_address = current_address->ifa_next;
+        current_interface = current_interface->ifa_next;
     }
 
-    /* TODO possibly use /proc/net/route */
+	/* interface not found */
+	printf("no interface found with address %s\n", target_addr);
 
-    freeifaddrs(addresses);
+    freeifaddrs(interfaces);
     return EXIT_SUCCESS;
 }

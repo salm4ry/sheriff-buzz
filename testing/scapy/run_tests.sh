953e90a090d5da8ff4b4ca68774820b14f5c83eb
Salma Berriche <salma.berriche@gmail.com>
2025-04-06 00:42:12 +0100

    Clean test run script
    - helper functions for starting/stopping sheriff-buzz, clearing
      capabilities
    - stop sheriff-buzz before running integration tests to clear blacklist
      and whitelists that could interfere with packets being sent
    - deactivate virtual environment after finishing tests

--- testing/scapy/run_tests.sh
+++ testing/scapy/run_tests.sh
@@ -20,9 +20,20 @@ integration_test_caps='CAP_NET_RAW=+ep'
 
 LOCALHOST=127.0.0.1
 
-# run sheriff-buzz on loopback interface, setting config file location
-echo "running ${prog}..."
-sudo -b "${dir}/${prog}" -i "${interface}" -c "${config_file}" -b "${bpf_obj}" -t
+start_buzz() {
+	# run sheriff-buzz on loopback interface, setting config file location
+	sudo -b "${dir}/${prog}" -i "${interface}" -c "${config_file}" -b "${bpf_obj}" -t
+}
+
+stop_buzz() {
+	sudo killall "${prog}"
+}
+
+clear_cap() {
+	# remove capabilities on .venv Python interpreter
+	sudo setcap -r "${venv_python}"
+}
+
 
 # activate virtual environment
 # (follow instructions in README.md to create the .venv directory)
@@ -30,15 +41,20 @@ source "${venv_dir}"/bin/activate
 
 # set unit testing capabilities
 sudo setcap "${unit_test_caps}" "${venv_python}"
+start_buzz
 # run unit tests
 "${venv_python}" unit_tests.py -u "${USER}" -t "${LOCALHOST}"
+stop_buzz  # required to clear blacklists and whitelists
+clear_cap
 
-# remove capabilities
-sudo setcap -r "${venv_python}"
+start_buzz
 # set new capabilities for integration testing
 sudo setcap "${integration_test_caps}" "${venv_python}"
 # run integration tests
+echo '-----'
 "${venv_python}" integration_tests.py -u "${USER}" -t "${LOCALHOST}"
+stop_buzz
+clear_cap
 
-
-sudo killall "${prog}"
+# deactivate virtual environment
+deactivate
